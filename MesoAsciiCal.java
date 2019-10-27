0eb7bc2652bfc34c596e9f55d522727775c64fad
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-26 20:59:14 -0500

    MesoAsciiCal: Variables, class var return, int calAverage created

--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -1,5 +1,22 @@
 
-public class MesoAsciiCal extends MesoAsciiAbstract 
-{
-   
+public class MesoAsciiCal extends MesoAsciiAbstract {
+	// Variables.
+	private MesoStation station;
+
+	// New constructor .
+	public MesoAsciiCal(MesoStation station) {
+		this.station = station;
+	}
+
+	@Override
+	int calAverage() {
+		// Variables.
+		double total = 0;
+		
+		// Loop that retrieves station ID from txt.
+		for(int i = 0; i < station.getStID().length(); i++) {
+			
+		}
+	}
+
 }

089550cb74d6d93f92f9d7bb4714a0a89c7b846a
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-26 21:44:35 -0500

    Made int-day hashedmap. loadDates also created.

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -5,9 +5,23 @@ import java.util.LinkedHashMap;
 public class DateTimeTwo {
 	// Private variable initialization.
 	private LinkedHashMap <LocalDate, Integer> dates = new LinkedHashMap <LocalDate, Integer>();
-	private LinkedHashMap<Integer, String> intToDay = new LinkedHashMap <Integer, String>();
-	
+	private LinkedHashMap <Integer, String> intToDay = new LinkedHashMap <Integer, String>();
+
 	public DateTimeTwo() throws IOException {
-		
+		intToDay.put(1, "SUNDAY");
+
+		intToDay.put(2, "MONDAY");
+
+		intToDay.put(3, "TUESDAY");
+
+		intToDay.put(4, "WEDNESDAY");
+
+		intToDay.put(5, "THURSDAY");
+
+		intToDay.put(6, "FRIDAY");
+
+		intToDay.put(7, "SATURDAY");
+
+		loadDates();
 	}
 }

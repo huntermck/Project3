5f463c6a245de756fea72078cd91af5eac9a51f5
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-26 21:04:08 -0500

    MesoAsciiCal: Working on loop to retrieve station ID

--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -3,7 +3,7 @@ public class MesoAsciiCal extends MesoAsciiAbstract {
 	// Variables.
 	private MesoStation station;
 
-	// New constructor .
+	// Returns current class instance variables.
 	public MesoAsciiCal(MesoStation station) {
 		this.station = station;
 	}
@@ -15,8 +15,19 @@ public class MesoAsciiCal extends MesoAsciiAbstract {
 		
 		// Loop that retrieves station ID from txt.
 		for(int i = 0; i < station.getStID().length(); i++) {
-			
+			total += station.getStID().charAt(i);
 		}
+		
+		double floor = Math.floor(total / station.getStID().length());
+		double ceil = Math.ceil(total / station.getStID().length());
+		
+		if(floor / (ceil - Math.floor(floor / ceil) < .25) ) {
+			return (int) floor;
+		} else if (floor / (ceil - Math.floor(floor / ceil)) >= .25) {
+			return (int) ceil;
+		}
+		
+		return -1; 
 	}
 
 }

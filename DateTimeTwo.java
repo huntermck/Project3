3c09c675b71830045ec46e981d11d66ed6e7173f
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-26 22:04:48 -0500

    Working on dateHashMapSorted, specifically sortedDates.

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -51,16 +52,33 @@ public class DateTimeTwo {
 		reader.close();
 	}
 
-	//TODO Description of constructor.
+	// TODO Description of constructor.
 	public void dateHashMap() {
-		//TODO Description of for loop.
+		// TODO Description of for loop.
 		for (Map.Entry<LocalDate, Integer> entry : dates.entrySet()) {
 			System.out.println(entry.getKey().toString() + ":" + entry.getValue());
 		}
 	}
-	
+
+	// TODO Description of constructor.
 	public void dateHashMapSorted() {
+		ArrayList <LocalDate> sortedDates = new ArrayList <LocalDate>();
+		LocalDate lowest = LocalDate.now();
+		
+		//TODO For loop description.
+		for (Map.Entry <LocalDate, Integer> entry : dates.entrySet()) {
+			sortedDates.add(entry.getKey());
+		}
+		
+		//TODO For loop description.
+		for (int i = 0; i < sortedDates.size(); i++) {
+			for (int j = i + 1; j < sortedDates.size(); j++) {
+				if(sortedDates.get(i).getYear() > sortedDates.get(j).getYear()) {
+					LocalDate temp = sortedDates.get(i);
+				}
+			}
+		}
 		
 	}
-	
+
 }

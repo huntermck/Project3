946ec7d1e4627c84da5f96719d58125a65da5c9b
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-26 21:15:14 -0500

    MesoLex: Created HashMap and Map. Also MesoLex variable retrieval.

--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class MesoLexicographical extends MesoSortedAbstract
 {
-
+	// Creates HashMap.
+	private HashMap <String, Integer> asciiVal;
+	
+	// Returns class instance variables.
+	public MesoLexicographical(HashMap <String, Integer> asciiVal) {
+		this.asciiVal = asciiVal;
+		
+		// Creates Map.
+		Map <String, Integer> sorted = sortedMap(asciiVal);
+	}
 }

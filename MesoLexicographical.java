1d042aaf84c6a0fa884ebf92bd25c6665cadf972
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-26 21:33:25 -0500

    Formatting of class. MesoLex: Working with sortedStations.

--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -2,38 +2,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MesoLexicographical extends MesoSortedAbstract
-{
+public class MesoLexicographical extends MesoSortedAbstract {
 	// Creates HashMap.
-	private HashMap <String, Integer> asciiVal;
-	
+	private HashMap<String, Integer> asciiVal;
+
 	// Returns class instance variables.
-	public MesoLexicographical(HashMap <String, Integer> asciiVal) {
+	public MesoLexicographical(HashMap<String, Integer> asciiVal) {
 		this.asciiVal = asciiVal;
-		
+
 		// Creates Map.
-		Map <String, Integer> sorted = sortedMap(asciiVal);
-		
-		//TODO Description.
-		for(Map.Entry <String, Integer> entry : sorted.entrySet()) {
+		Map<String, Integer> sorted = sortedMap(asciiVal);
+
+		// TODO Description.
+		for (Map.Entry<String, Integer> entry : sorted.entrySet()) {
 			System.out.println(entry.getKey());
 		}
 	}
-	
-	//TODO Description of constructor.
+
+	// TODO Description of constructor.
 	@Override
-	public Map <String, Integer> sortedMap(HashMap <String, Integer> unsorted) {
+	public Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted) {
 		ArrayList<String> sortedStations = new ArrayList<String>();
 		HashMap<String, Integer> sorted = new HashMap<String, Integer>();
-		
-		//TODO Description of for loop.
-		for (Map.Entry <String, Integer> entry: unsorted.entrySet()) {
+
+		// TODO Description of for loop.
+		for (Map.Entry<String, Integer> entry : unsorted.entrySet()) {
 			sortedStations.add(entry.getKey());
 		}
-		
-		//TODO Description of for loop.
-		for(int i = 0; i < sortedStations.size(); i++) {
-			
+
+		// TODO Description of for loop.
+		for (int i = 0; i < sortedStations.size(); i++) {
+			for (int j = i + 1; j < sortedStations.size(); j++) {
+
+				if ((int) sortedStations.get(i).charAt(0) < (int) sortedStations.get(j).charAt(0)) {
+					String temp = sortedStations.get(i);
+					sortedStations.set(i, sortedStations.get(j));
+					sortedStations.set(j, temp);
+
+				}
+			}
 		}
 	}
 }

43e8c032528243f9f00b154bf066b3cc3aefdc74
Hunter McKinnon <Hunter.R.McKinnon-1@ou.edu>
2019-10-26 22:14:52 -0500

    Working on loadStations constr.

--- DateSortingUsingAlgorithm.java
+++ DateSortingUsingAlgorithm.java
@@ -1,13 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 
 public class DateSortingUsingAlgorithm {
 	// Private variables initilization.
 	private LinkedHashMap <LocalDate, Integer> stations = new LinkedHashMap <LocalDate, Integer>();
 
-	//TODO
+	//TODO Description.
 	public DateSortingUsingAlgorithm() throws IOException {
 
 	}
+
+	//TODO Description.
+	private void loadStations() throws IOException {
+		BufferedReader reader = new BufferedReader(
+				new FileReader("C:\\Users\\hunte\\eclipse-workspace\\Project3\\src\\SortingDates.txt"));
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-M-dd");
+	}
+	
 }
